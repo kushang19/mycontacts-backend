0c33639a341055d51e03675e70daaab7a93fc505
Kushang Vijay Gharat <kushang81@gmail.com>
2024-11-16 23:41:04 +0530

    user register

---
+++ controller/userController.js
@@ -0,0 +1,65 @@
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt")
+
+const User = require("../models/userModel");
+
+// @desc Register a user 
+// @route POST /api/users/register
+// @access public
+
+const registerUser = asyncHandler(async (req, res) => {
+    const {username, email, password} = req.body;
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error("All fields are mandetory!")
+    }
+    const userAvailable = await User.findOne({email});
+
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User already Registered!")
+    }
+
+    // Hash Password 
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("Hashed Password: ", hashedPassword);
+
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword
+    });
+
+    console.log(`User Created  ${user}`);
+    if(user){
+        res.status(201).json({_id: user.id, email: user.email});
+    }
+    else{
+        res.status(400)
+        throw new Error("User data is not vaild")
+    }
+        
+    res.json({
+        message: "Register the User"
+    })
+});
+
+// @desc Login user 
+// @route POST /api/users/login
+// @access public
+const loginUser = asyncHandler(async (req, res) => {
+    res.json({
+        message: "Login"
+    })
+});
+
+// @desc Current user 
+// @route POST /api/users/current
+// @access private
+const currentUser = asyncHandler(async (req, res) => {
+    res.json({
+        message: "Current User Information"
+    })
+});
+
+module.exports = { registerUser, loginUser, currentUser }
