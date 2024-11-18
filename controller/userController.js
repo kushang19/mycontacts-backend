d07d87e4d6b42c2ea3fb150333ff1aacf222be87
Kushang Vijay Gharat <kushang81@gmail.com>
2024-11-19 00:26:33 +0530

    Validate token handler

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/userModel");
 
@@ -48,18 +49,36 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/login
 // @access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({
-        message: "Login"
-    })
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        res.status(400);
+        throw new Error("All fields are mandetory!!");
+    };
+    const user = await User.findOne({email});
+    if(user && (await bcrypt.compare(password, user.password))){
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id
+            }
+        },  
+        process.env.ACCESS_TOKEN_SECERT, 
+        {expiresIn: "1m"});
+        res.status(200).json({accessToken})
+    }else{
+        res.status(401);
+        throw new Error("email or password is not valid");
+    }
+    res.json({ message: "Login" })
 });
 
 // @desc Current user 
 // @route POST /api/users/current
 // @access private
 const currentUser = asyncHandler(async (req, res) => {
-    res.json({
-        message: "Current User Information"
-    })
+    res.json(req.user)
 });
 
 module.exports = { registerUser, loginUser, currentUser }
